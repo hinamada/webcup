f2be7768dfa8fea2a105bc69be471059e9945af2
Mendrika RALISON <ralisonmendrika@gmail.com>
2019-06-15 21:50:41 +0300

    Informations

--- application/controllers/Info.php
+++ application/controllers/Info.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Informations extends CI_Controller {
+class Info extends CI_Controller {
 
     /**
      * Index Page for this controller.
