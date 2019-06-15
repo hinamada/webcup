2e97f4cc1e9e3093a43e23f8eb36d3e351d257b2
mevaElodie <rr.mevaelodie@gmail.com>
2019-06-16 01:36:19 +0200

    nanova chemin

--- application/config/config.php
+++ application/config/config.php
@@ -24,8 +24,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |
 */
 
-/*$config['base_url'] = 'https://hinawebcup.herokuapp.com';*/
-$config['base_url'] = 'http://localhost/webcup/';
+$config['base_url'] = 'https://hinawebcup.herokuapp.com';*/
+//$config['base_url'] = 'http://localhost/webcup/';
 
 
 /*
